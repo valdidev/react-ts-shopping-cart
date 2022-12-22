eb1338b48874ca71164a153b10133d101e3f07fb
valdidev <iamvaldidev@gmail.com>
2022-12-22 11:42:53 +0100

    feat: getProducts fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import { useState } from 'react';
-import { useQuery  } from 'react-query';
+import { useState } from "react";
+import { useQuery } from "react-query";
 
-import Drawer from '@material-ui/core/Drawer';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import Grid from '@material-ui/core/Grid';
-import AddShoppingCart from '@material-ui/icons/AddShoppingCart';
-import Badge from '@material-ui/core/Badge';
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCart from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
 
-import { Wrapper } from './App.styles';
+import { Wrapper } from "./App.styles";
 
+
+const getProducts = async () =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div>App</div>;
+};
 
 export default App;
