1f14eab3351ef90a6b65b42b2ab7dec79298170b
valdidev <iamvaldidev@gmail.com>
2022-12-22 11:37:03 +0100

    chore: basic app imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { useState } from 'react';
+import { useQuery  } from 'react-query';
+
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCart from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+
+
 const App = () => {
   return (
     <div>App</div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(<App />);
+const client = new QueryClient();
+
+root.render(
+  <QueryClientProvider client={client}>
+    <App />
+  </QueryClientProvider>
+);
